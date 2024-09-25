e2b9e9966bb25a5950f5fcbc07bc5a789d33c959
ferdie bergado <ferdiebergado@gmail.com>
2024-09-25 14:20:07 +0800

    Refactored to retrieve db pass from .env file

--- startdb.sh
+++ startdb.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 
 if [ -e $DB_PASS ]; then
+    if [ -f .env ]; then
+        DB_PASS=$(awk -F '=' '$1 == "DB_PASS" { print $2 }' .env)
+    fi
+fi
+
+if [ -e $DB_PASS ]; then
     echo "DB_PASS not set"
     exit 1
 fi
@@ -8,11 +14,11 @@ fi
 container=$(command -v docker)
 podman=$(command -v podman)
 
-if [ -n $podman ]; then 
+if [ -n $podman ]; then
     container=$podman
-fi 
+fi
 
-$container run -ti --rm -p 5432:5432 --name ptms_postgres  \
+$container run -ti --rm -p 5432:5432 --name ptms_postgres \
     -e POSTGRES_PASSWORD="$DB_PASS" \
     -v ./postgresql.conf:/etc/postgresql/postgresql.conf:Z \
     postgres:16-alpine3.20 \
