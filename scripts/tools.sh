34ec1a108e14da080115a6811582a62c223936c8
ferdie bergado <ferdiebergado@gmail.com>
2024-09-28 17:52:46 +0800

    Moved scripts to own dir

--- scripts/tools.sh
+++ scripts/tools.sh
@@ -2,3 +2,5 @@
 
 go install -tags 'postgres' github.com/golang-migrate/migrate/v4/cmd/migrate@latest
 go install github.com/sqlc-dev/sqlc/cmd/sqlc@latest
+go install github.com/go-task/task/v3/cmd/task@latest
+go install github.com/air-verse/air@latest
