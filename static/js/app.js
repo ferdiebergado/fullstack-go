427308df22c6adeccdb4a13fd22d7abf90b98554
ferdie bergado <ferdiebergado@gmail.com>
2024-10-29 18:51:31 +0800

    Updated .gitignore to add static dir

--- static/js/app.js
+++
@@ -1,2 +0,0 @@
-var e=".navbar a",n="active";function s(){let a="/"+window.location.pathname.split("/")[1];document.querySelectorAll(e).forEach(t=>{new URL(t.href).pathname===a&&a!=="/"?t.classList.add(n):t.classList.remove(n)})}s();
-//# sourceMappingURL=app.js.map
