e8781c193cc72807f4a4f80921dc5617013fd8d8
ferdie bergado <ferdiebergado@gmail.com>
2024-10-31 18:03:48 +0800

    Enabled content check

--- pkg/http/error_handler.go
+++ pkg/http/error_handler.go
@@ -3,6 +3,7 @@ package http
 import (
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/ferdiebergado/fullstack-go/internal/ui"
 )
@@ -11,13 +12,20 @@ import (
 func ErrorHandler(w http.ResponseWriter, r *http.Request, status int, msg string, err error) {
 	log.Printf("%s: %v\n", msg, err)
 
+	// Check the "Accept" header for preferred content type
+	acceptHeader := r.Header.Get("Accept")
+
 	switch status {
 	case http.StatusBadRequest:
 		http.Error(w, "Bad Request", http.StatusBadRequest)
 	case http.StatusForbidden:
 		http.Error(w, "Forbidden", http.StatusForbidden)
 	case http.StatusNotFound:
-		// http.Error(w, "Not Found", http.StatusNotFound)
+		if prefersJSON(acceptHeader) {
+			http.NotFound(w, r)
+			return
+		}
+
 		err := ui.RenderHTML(w, "404.html", nil)
 
 		if err != nil {
@@ -29,4 +37,16 @@ func ErrorHandler(w http.ResponseWriter, r *http.Request, status int, msg string
 	default:
 		http.Error(w, "An Error Occurred", status)
 	}
+}
+
+// prefersJSON checks if "application/json" is present and prioritized in the Accept header
+func prefersJSON(acceptHeader string) bool {
+	mediaTypes := strings.Split(acceptHeader, ",")
+	for _, mediaType := range mediaTypes {
+		// Trim spaces and check if the media type is "application/json"
+		if strings.TrimSpace(mediaType) == "application/json" {
+			return true
+		}
+	}
+	return false
 }
