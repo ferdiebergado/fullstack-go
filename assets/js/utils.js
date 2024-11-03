58f1c4500d4e660ab6a95f47d0ce10aca96f95ab
ferdie bergado <ferdiebergado@gmail.com>
2024-11-04 07:37:11 +0800

    Moved sanitize to utils module

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -59,3 +59,29 @@ export function highlightText(text, searchTerm) {
 
   return highlightedText;
 }
+
+/**
+ * Converts special HTML characters into their corresponding HTML entities,
+ * which helps prevent XSS (Cross-Site Scripting) attacks
+ * when displaying user-generated content.
+ *
+ * @param {FormDataEntryValue|string} input
+ * @returns {string|Blob}
+ */
+export function sanitize(input) {
+  if (typeof input === 'string') {
+    // Create a div element to leverage the browser's HTML parsing
+    const div = document.createElement('div');
+
+    // Set the text content of the div to the input value
+    // This automatically escapes any HTML tags
+    div.textContent = input;
+
+    // Return the sanitized string
+    return div.innerHTML;
+  } else if (input instanceof File) {
+    return input;
+  }
+
+  return '';
+}
